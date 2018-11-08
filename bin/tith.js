021b8130636bc0a28ffeadf865c4e46658ae73e9
Rampancy <zakattak47@gmail.com>
2018-11-08 10:25:20 -0500

    Fixed deprecation warning [DEP0013] that throws type error in Node version 10+

--- bin/tith.js
+++ bin/tith.js
@@ -31,7 +31,9 @@ function createTiAppFile(fromPath) {
 
 function copyFile(fromPath, toPath) {
     fs.readFile(fromPath, function (err, data) {
-        fs.writeFile(toPath, data);
+        fs.writeFile(toPath, data, (err) => {
+          if(err) throw err;
+        });
     });
 }
 
